dc5c942efd14097e7402fd41ec77a2c6735007f8
Harsh <hbanthiya@gmail.com>
2022-09-29 15:45:30 -0400

    added the class template that needs to be implemented for vectors

---
+++ Ft_vector/ft_vector.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vector.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 15:29:19 by hbanthiy          #+#    #+#             */
+/*   Updated: 2022/09/29 15:41:14 by hbanthiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_VECTOR_HPP
+#define FT_VECTOR_HPP
+
+/* PSEUDO CODE 
+
+Includes = <compare> <initializer list>
+Constructor = Vector (with a polymorphic allocator)
+
+Hash something
+ 
+Operators 
+== 
+!= 
+< 
+<= 
+>
+>=
+
+Functions 
+swap
+erase()
+eraseif()
+
+Range access 
+begin()
+cbegin()
+
+end()
+cend()
+
+rbegin()
+crbegin()
+
+rend()
+crend()
+
+size()
+ssize()
+
+empty()
+data()
+
+*/
+
+
+#endif
