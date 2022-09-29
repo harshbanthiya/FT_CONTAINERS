0d0bf8cd3bfb601f1c3322ea2206df28af847029
Harsh <hbanthiya@gmail.com>
2022-09-29 12:38:55 -0400

    build system and unit testing functional

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hbanthiy/FT_CONTAINERS
-# Build directory: /Users/hbanthiy/FT_CONTAINERS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hbanthiy/FT_CONTAINERS/tests
-# Build directory: /Users/hbanthiy/FT_CONTAINERS/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[ExampleTests]=] "/Users/hbanthiy/FT_CONTAINERS/build/tests/ExampleTests")
-set_tests_properties([=[ExampleTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/hbanthiy/FT_CONTAINERS/tests/CMakeLists.txt;15;add_test;/Users/hbanthiy/FT_CONTAINERS/tests/CMakeLists.txt;0;")
