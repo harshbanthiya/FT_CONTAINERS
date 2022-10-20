c1397c67d6bd7707724b4001008c59d00db55c29
Harsh <hbanthiya@gmail.com>
2022-10-20 14:04:09 -0400

    working

--- _main.cpp
+++ _main.cpp
@@ -6,6 +6,10 @@
 int main()
 {
     int n = 3;
-    
+
+        ft::vector<int> v1;
+        ft::vector<int> v2(10);
+        ft::vector<char> v3(n, 'a');
     ft::vector<std::string> a (n, "Hello world");
+    ft::vector<int> v8, v9, v10;
 }
