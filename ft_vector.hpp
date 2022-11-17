5dfbedb25ecb68edf41b15a49adfa2b910bb348e
Harsh <hbanthiya@gmail.com>
2022-11-17 13:26:31 -0500

    new vectors

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -1,216 +1,123 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_vector.hpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 15:29:19 by hbanthiy          #+#    #+#             */
-/*   Updated: 2022/11/17 11:40:58 by hbanthiy         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FT_VECTOR_HPP
-#define FT_VECTOR_HPP
+# define FT_VECTOR_HPP
 
-#include <iostream>
-#include <type_traits>
-#include <algorithm>
-#include <vector>
-#include <memory>
-#include <stdexcept>
-#include "Iterator.hpp"
-#include <iterator>
+# include <memory>
+# include "Iterator.hpp"
 
+#define FT_NOEXCEPT throw()
 
 namespace ft
 {
-    template<typename T, typename Allocator = std::allocator<T> > 
-    class vector
-    {
-        
-        public:
+	template<typename iter>
+	class vector_iter
+	{
+		public:
 
-        typedef Allocator                                       allocator_type;
-        typedef  T                                              value_type;
-        typedef typename allocator_type::pointer                pointer;
-        typedef typename allocator_type::const_pointer          const_pointer;
-        typedef typename allocator_type::reference              reference;
-        typedef typename allocator_type::const_reference        const_reference;
-        typedef typename allocator_type::size_type              size_type;
-        typedef typename allocator_type::difference_type        difference_type;
-        typedef value_type*                                     iterator;
-        typedef const value_type*                               const_iterator;
-    
-        /*-----------------------------------------------------------------------------*/
-        /* Constructors and destructor *************************************************/
-        /*-----------------------------------------------------------------------------*/
+			typedef iter 																iterator_type;
+			typedef typename ft::iterator_traits<iterator_type>::iterator_category		iterator_category;
+			typedef typename ft::iterator_traits<iterator_type>::value_type 			value_type;
+			typedef typename ft::iterator_traits<iterator_type>::difference_type 		difference_type;
+			typedef typename ft::iterator_traits<iterator_type>::pointer 				pointer;
+			typedef typename ft::iterator_traits<iterator_type>::reference 				reference;
 
-        vector();
-        ~vector();
-        vector(vector const& rhs);
+			// Constructor 
+			vector_iter() FT_NOEXCEPT {}
+			vector_iter(iterator_type x) FT_NOEXCEPT : it(x) {}
 
-        explicit vector(size_type n); 
-        explicit vector(size_type n, const value_type& val);
-        template<class InputIterator>
-        vector(InputIterator first, InputIterator last);
+			iterator_type base() const FT_NOEXCEPT {return it;}
 
-        /*******************************************************************************/
-        /*/////////////////////////////////////////////////////////////////////////////*/
-        /*******************************************************************************/
+			// Operators 
+			
+			reference	operator*() const FT_NOEXCEPT {return *it;}
+			pointer 	operator->() const FT_NOEXCEPT {return it;}
 
-        /*-----------------------------------------------------------------------------*/
-        /* Overloaded operators ********************************************************/
-        /*-----------------------------------------------------------------------------*/
+			vector_iter& operator++() FT_NOEXCEPT {++it; return *this;}
+			vector_iter  operator++(int) FT_NOEXCEPT {vector_iter tmp(*this); ++(*this); return (tmp);}
 
-        vector<T, Allocator>&           operator=(vector<T, Allocator> const &rhs);
-        reference                       operator[](size_type index);
-        const_reference                 operator[](size_type index) const;
-        
-        template <vector<T, 
-        bool                            operator==(const vector<T, Allocator> &lhs, const vector<T, Allocator> &rhs);
-        bool                            operator<(const vector<T, Allocator> &lhs, const vector<T, Allocator> &rhs);
-        bool                            operator!=(const vector<T, Allocator> &lhs, const vector<T, Allocator> &rhs);
-        bool                            operator>(const vector<T, Allocator> &lhs, const vector<T, Allocator> &rhs);
-        bool                            operator<=(const vector<T, Allocator> &lhs, const vector<T, Allocator> &rhs);
-        bool                            operator>=(const vector<T, Allocator> &lhs, const vector<T, Allocator> &rhs);
+			vector_iter& operator--() FT_NOEXCEPT {--it; return *this;}
+			vector_iter  operator--(int) FT_NOEXCEPT {vector_iter tmp(*this); --(*this); return (tmp);}
 
-        /*******************************************************************************/
-        /*/////////////////////////////////////////////////////////////////////////////*/
-        /*******************************************************************************/
-
-        /*-----------------------------------------------------------------------------*/
-        /* Member functions for accessing value ****************************************/
-        /*-----------------------------------------------------------------------------*/
-        
-        reference                       at(size_type index){validateIndex(index); return ((*this)[index]);}
-        const_reference                 at(size_type index) const{validateIndex(index); return ((*this)[index]);}
-        reference                       front(){return (*_begin);}
-        const_reference                 front() const{return (*_begin);}
-        reference                       back(){return (*(_length - 1));}
-        const_reference                 back() const{return (*(_length - 1));}
-
-        //reverse_iterator              rbegin(){return (reverse_iterator(end()));};
-        const_iterator                  begin() const{return (_begin);}
-        //const_reverse_iterator        rbegin() const{return (const_riterator(end()));};
-
-        iterator                        end(){return (_length);}
-        //reverse_iterator              rend(){return (reverse_iterator(begin()));};
-        const_iterator                  end() const{return (_length);}
-        //const_reverse_iterator        rend() const{return (const_reverse_iterator(begin()));};
-
-        const_iterator                  cbegin() const{return (begin());};
-        //const_reverse_iterator        crbegin() const{return (rbegin());};
-        const_iterator                  cend() const{return (end());};
-        //const_reverse_iterator        crend() const{return (rend());};
-
-        /*******************************************************************************/
-        /*/////////////////////////////////////////////////////////////////////////////*/
-        /*******************************************************************************/
-
-        /*-----------------------------------------------------------------------------*/
-        /* Member functions for reporting status ***************************************/
-        /*-----------------------------------------------------------------------------*/
-
-        size_type                       size()  const;
-        size_type                       max_size()  const;
-        size_type                       capacity()  const;
-        bool                            empty()  const;
-
-        /******************************************************************************/
-        /*////////////////////////////////////////////////////////////////////////////*/
-        /******************************************************************************/
-
-        /*----------------------------------------------------------------------------*/
-        /* Member functions for inserting one or more values **************************/
-        /*----------------------------------------------------------------------------*/
-
-        template<class Iter>
-        void                            assign(Iter first, Iter last);
-        void                            assign(size_type n, const value_type& value);
-
-        template<class Iter>
-        iterator                        insert(const_iterator pos, Iter first, Iter last);
-        iterator                        insert(const_iterator pos, const value_type& value);
-        iterator                        insert(const_iterator pos, value_type& value);
-        iterator                        insert(const_iterator pos, size_type n, const value_type& value);
-
-        void                            push_back(const value_type& value);
-        void                            push_back(value_type& value);
+			vector_iter operator+(difference_type n) const FT_NOEXCEPT {vector_iter tmp(*this); tmp += n; return (tmp);}
+			vector_iter operator+=(difference_type n) FT_NOEXCEPT {it += n; return (*this);}
+			vector_iter operator-(difference_type n) const FT_NOEXCEPT {return (*this + (-n));}
+			vector_iter operator-=(difference_type n) FT_NOEXCEPT {*this += -n; return (*this);}
+			
+			reference 	operator[](difference_type n) const FT_NOEXCEPT {return it[n];}
 
 
-        /******************************************************************************/
-        /*////////////////////////////////////////////////////////////////////////////*/
-        /******************************************************************************/
+ 		private:
 
-        /*----------------------------------------------------------------------------*/
-        /* Member functions for deleting one or more values ***************************/
-        /*----------------------------------------------------------------------------*/
+  			iterator_type																it;
 
-        iterator                        erase(const_iterator pos);
-        iterator                        erase(const_iterator first, const_iterator last);
-        void                            clear(){erase(begin(), end());}
-        void                            pop_back();
+	};
 
-        /******************************************************************************/
-        /*////////////////////////////////////////////////////////////////////////////*/
-        /******************************************************************************/
 
-        /*----------------------------------------------------------------------------*/
-        /* Miscellaneous member functions *********************************************/
-        /*----------------------------------------------------------------------------*/
-        
-        void                            swap(vector& other);
-        void                            reserve(size_type capacityUpperBound);
-        void                            resize(size_type new_size, const value_type& val);
-        void                            resize(size_type new_size){ return resize(new_size, value_type()); }
-        
-        /******************************************************************************/
-        /*////////////////////////////////////////////////////////////////////////////*/
-        /******************************************************************************/        
+	template<typename Iter1, typename Iter2>
+	inline bool operator==(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return (x.base() == y.base());}
+	template<typename Iter1, typename Iter2>
+	inline bool operator!=(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return !(x.base() == y.base());}
 
-        /*----------------------------------------------------------------------------*/
-        /* Private variables and functions ********************************************/
-        /*----------------------------------------------------------------------------*/
-        private:
-        
-        allocator_type                  _alloc;
-        iterator                        _begin; 
-        iterator                        _length; 
-        iterator                        _capacity;
-        
-        void                            fill_and_initialise(size_type n, const value_type& val);
-        void                            initialise_space(size_type n, size_type cap);
-        template<class Iter>
-        void                            range_initialise(Iter first, Iter last);
-        void                            destroy_and_recover(iterator first, iterator last, size_type n);
-        
-        template<class Iiter>
-        void                            copy_and_assign(Iiter first, Iiter last, input_iterator_tag);
-        void                            fill_and_assign(size_type n, const value_type& value);
-        
-        template<class Iiter>
-        void                            copy_and_insert(iterator pos, Iiter first, Iiter last);
-        iterator                        fill_and_insert(iterator pos, size_type n, const value_type& val);
-        void                            reallocate_insert(iterator pos, const value_type& val);
+	template<typename Iter1, typename Iter2>
+	inline bool operator<(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return (x.base() < y.base());}
+	template<typename Iter1, typename Iter2>
+	inline bool operator>(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return (y < x);}
+	template<typename Iter1, typename Iter2>
+	inline bool operator<=(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return !(y < x);}
+	template<typename Iter1, typename Iter2>
+	inline bool operator>=(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return !(x < y);}
 
-        void                            resize_if_req();
-        void                            pushBackInternal(T const &value);
-        void                            validateIndex(size_type index) const {if (index >= _length) throw std::out_of_range("Out of Range");};
-        void                            reserveCapacity(size_type newCapacity);
-        
-        size_type                       get_new_capacity(size_type add_size);
+	//template<typename Iter1, typename Iter2>
+	//inline bool operator-(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return (x.base() < y.base());}
+	//template<typename Iter1, typename Iter2>
+	//inline bool operator>(const vector_iter<Iter1>& x, const vector_iter<Iter2>& y) FT_NOEXCEPT {return (y < x);}
 
-        /******************************************************************************/
-        /*////////////////////////////////////////////////////////////////////////////*/
-        /******************************************************************************/
 
-        /*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+	template <typename T, typename Allocator = std::allocator<T>()>
+	class vector
+	{
+		public:
 
-    };    
-        
-} // namespace ft   
+			typedef T                                        	value_type;
+			typedef Allocator                                	allocator_type;
+			typedef typename allocator_type::reference       	reference;
+			typedef typename allocator_type::const_reference 	const_reference;
+			typedef typename allocator_type::size_type       	size_type;
+			typedef typename allocator_type::difference_type 	difference_type;
+			typedef typename allocator_type::pointer         	pointer;
+			typedef typename allocator_type::const_pointer   	const_pointer;
 
-#include "ft_vector.cpp"
+			typedef vector_iter									iterator;
+			typedef vector_iter                   				const_iterator;
+			typedef ft::reverse_iterator<iterator>          	reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>    	const_reverse_iterator;
 
-#endif
+
+			// Constructor, Destructor and related functions. | * |  
+
+			vector(const Allocator& = Allocator()); // Default 
+			explicit vector(size_type n, const T& value = T(), const Allocator& = Allocator()); // Vector of size n with value 
+			
+			vector(const vector<T, Allocator>& x); // Copy constructor 
+			
+			template <typename InputIterator>
+			vector(InputIterator first, InputIterator last, const Allocator& = Allocator()); // Range based initialization 
+																							 // Construct vector last - first [first, last)		
+
+			vector<T, Allocator>&		operator=(const vector<T, Allocator> &x); // Assignment operator  
+
+			~vector(); // Destructor 
+
+			// Assign 
+			template<typename InputIterator>
+			void assign(InputIterator first, InputIterator last); // == {clear(); insert(begin(), first, last);}
+			void assign(size_type n, const T& u);
+
+			void reserve(size_type n);
+			void swap(vector<T, Allocator>& x);
+
+		private:
+
+	};
+}
+
+#endif /* ******************************************************* FT_VECTOR_H */
