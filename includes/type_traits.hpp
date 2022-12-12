bc7e4f580389abd49159df413b6e5bbe4b7da439
Harsh <hbanthiya@gmail.com>
2022-12-12 13:36:04 -0500

    refactored type_traits and added explicit public inheritance

--- includes/type_traits.hpp
+++ includes/type_traits.hpp
@@ -6,14 +6,12 @@
 /*   By: hbanthiy <hbanthiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 10:43:24 by hbanthiy          #+#    #+#             */
-/*   Updated: 2022/11/28 11:06:14 by hbanthiy         ###   ########.fr       */
+/*   Updated: 2022/12/12 13:35:38 by hbanthiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TYPE_TRAITS_HPP
-# define TYPE_TRAITS_HPP
-
-#include <cstddef>
+#ifndef TYPE_TRAITS
+# define TYPE_TRAITS
 
 #define FT_NOEXCEPT throw()
 
@@ -22,13 +20,12 @@ namespace ft
     template <typename Type, Type V>
     struct integral_constant
     {
+        typedef integral_constant<Type, V>      type;
+        typedef Type                            value_type;
+       
         static const Type value = V;
-
-        typedef Type               value_type;
-        typedef integral_constant   type;
-
+        
         operator value_type() const FT_NOEXCEPT {return V;}
-        const value_type operator() () const FT_NOEXCEPT {return V;}
     };
 
     typedef integral_constant<bool, true> true_type;
@@ -55,31 +52,29 @@ namespace ft
     template <typename T>
     struct remove_cv {typedef typename _remove_cv<T>::type type;};
 
-    template <class T>  struct _is_integral                        : false_type {};
-    template <>         struct _is_integral<bool>                  : true_type {};
-    template <>         struct _is_integral<char>                  : true_type {};
-    template <>         struct _is_integral<signed char>           : true_type {};
-    template <>         struct _is_integral<unsigned char>         : true_type {};
-    template <>         struct _is_integral<wchar_t>               : true_type {};
-    template <>         struct _is_integral<char16_t>              : true_type {};
-    template <>         struct _is_integral<char32_t>              : true_type {};
-    template <>         struct _is_integral<short>                 : true_type {};
-    template <>         struct _is_integral<unsigned short>        : true_type {};
-    template <>         struct _is_integral<int>                   : true_type {};
-    template <>         struct _is_integral<unsigned int>          : true_type {};
-    template <>         struct _is_integral<long>                  : true_type {};
-    template <>         struct _is_integral<unsigned long>         : true_type {};
-    template <>         struct _is_integral<long long>             : true_type {};
-    template <>         struct _is_integral<unsigned long long>    : true_type {};
+    template <class T>  struct _is_integral                        : public false_type {};
+    template <>         struct _is_integral<bool>                  : public true_type {};
+    template <>         struct _is_integral<char>                  : public true_type {};
+    template <>         struct _is_integral<signed char>           : public true_type {};
+    template <>         struct _is_integral<unsigned char>         : public true_type {};
+    template <>         struct _is_integral<wchar_t>               : public true_type {};
+    template <>         struct _is_integral<short>                 : public true_type {};
+    template <>         struct _is_integral<unsigned short>        : public true_type {};
+    template <>         struct _is_integral<int>                   : public true_type {};
+    template <>         struct _is_integral<unsigned int>          : public true_type {};
+    template <>         struct _is_integral<long>                  : public true_type {};
+    template <>         struct _is_integral<unsigned long>         : public true_type {};
+    template <>         struct _is_integral<long long>             : public true_type {};
+    template <>         struct _is_integral<unsigned long long>    : public true_type {};
 
     template <typename T>
-    struct is_integral : _is_integral<typename remove_cv<T>::type> {};
+    struct is_integral : public _is_integral<typename remove_cv<T>::type> {};
     
     template <typename T, typename U>
-    struct is_same : false_type {};
+    struct is_same : public false_type {};
 
     template <typename T>
-    struct is_same<T, T> : true_type {};
+    struct is_same<T, T> : public true_type {};
 
     template <typename>
     struct void_t {typedef void type;};
